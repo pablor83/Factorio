e4e298a8043d5f409cb944f27e1b68506ca8ab31
PrzemekKarpisz <54801786+PrzemekKarpisz@users.noreply.github.com>
2020-06-17 15:14:49 +0200

    1
    1

--- project/src/main/java/com/project/LoginWindow/LoginWindowController.java
+++ project/src/main/java/com/project/LoginWindow/LoginWindowController.java
@@ -5,15 +5,28 @@ import javafx.fxml.FXML;
 
 public class LoginWindowController {
 
+	@FXML
+	private String userName;
 	
+	@FXML
+	private String UserPass;
 	
 	
 	
 	
 	@FXML
-	public void cancelButton() {
+	public void cancelButtonClick() {
 		Platform.exit();
 	}
+	
+	
+	@FXML
+	public static void OkButtonClick() {
+		
+	}
+	
+	
+	
 	
 	
 }
