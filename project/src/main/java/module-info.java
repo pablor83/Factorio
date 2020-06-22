f2ed2209b79f0232ded9aa354618f7f4b67fc74b
PrzemekKarpisz <54801786+PrzemekKarpisz@users.noreply.github.com>
2020-06-22 15:23:57 +0200

    pom aktualizaction
    pom aktualization

--- project/src/main/java/module-info.java
+++ project/src/main/java/module-info.java
@@ -1,13 +1,19 @@
 module JavaFX {
 	exports com.project.LoginWindow;
-
+	
+	
+	
+	
     requires javafx.fxml;
     requires javafx.controls;
 	requires javafx.base;
 	requires javafx.graphics;
 	requires java.sql;
 	
+	
+	
 	opens com.project.LoginWindow to javafx.fxml;
+	
 	
 	
    
